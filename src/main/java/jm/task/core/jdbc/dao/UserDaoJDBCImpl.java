ebfce722fc2d58c45e554d2346fe0f8a31e05d9a
e-terven <dparentco@gmail.com>
2023-01-31 23:00:40 +0900

    branch 1.1.4 - done

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,6 +3,8 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl extends Util implements UserDao {
@@ -12,26 +14,87 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
     }
 
     public void createUsersTable() {
+        try (Connection con = Util.getConnection();
+             Statement statement = con.createStatement()) {
+            statement.executeUpdate("""
+                    CREATE TABLE IF NOT EXISTS users (
+                    id SERIAL PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                    name VARCHAR(25), 
+                    lastName VARCHAR(25), 
+                    age INT);
+                    """);
+            System.out.println("Table Users created successfully");
 
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Connection con = Util.getConnection();
+             Statement statement = con.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS users;");
+            System.out.println("Table has been deleted");
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try (Connection con = Util.getConnection();
+             PreparedStatement preparedStatement = con.prepareStatement("""
+            INSERT INTO users (name, lastName, age) 
+            VALUES (?, ?, ?);
+            """)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
 
+            System.out.println("Added User: " + " " + name  + " " + lastName  + " " + age);
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Connection con = Util.getConnection();
+             PreparedStatement preparedStatement = con.prepareStatement("DELETE FROM users WHERE id = ?")) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new ArrayList<>();
+        try (Connection con = Util.getConnection();
+             PreparedStatement preparedStatement = con.prepareStatement("SELECT * FROM users;")) {
+            ResultSet res = preparedStatement.executeQuery();
+            while (res.next()) {
+                User user = new User();
+                user.setId(res.getLong(1));
+                user.setName(res.getString(2));
+                user.setLastName(res.getString(3));
+                user.setAge(res.getByte(4));
+                list.add(user);
+            }
+            System.out.println('\n' + "Users list:");
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+        return list;
     }
 
     public void cleanUsersTable() {
+        try (Connection con = Util.getConnection();
+             Statement statement = con.createStatement()) {
+            statement.executeUpdate("DELETE FROM users;");
+            System.out.println('\n' + "Table is empty");
 
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,9 +2,13 @@ package jm.task.core.jdbc.util;
 
 import com.mysql.jdbc.Driver;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.lang.String;
 
 public class Util {
     // реализуйте настройку соеденения с БД
@@ -12,25 +16,47 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/new_schema";
     private static final String USER = "root";
     private static final String PASSWORD = "mysql33!";
+    private static final String URL_KEY = "db.url";
+    private static final String USER_KEY = "db.user";
+    private static final String PASSWORD_KEY = "db.password";
+
+    private static final Properties PROPERTIES = new Properties();
 
     static {
         loadDriver();
+        //loadProperties();
     }
     public static void loadDriver() {
         try {
             Class.forName(DRIVER); // загрузка в память JVM драйвера для работы с БД
-            // https://youtu.be/U7Pm3Vi0aWU?t=351
+
         } catch (ClassNotFoundException e) {
-            // e.printStackTrace(); - тогда приложение продолжит
+            //e.printStackTrace(); // тогда приложение продолжит
             throw new RuntimeException(e);
         }
     }
+
+    public static void loadProperties () {
+        try (InputStream inputStream = Util.class.getClassLoader().getResourceAsStream("application.properties")) {
+            PROPERTIES.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static String get (String key) {
+        return PROPERTIES.getProperty(key);
+    }
+
     public static Connection getConnection() {
         Connection con = null;
-        // Class <Driver> driverClass = Driver.class;
-        // в старрых версиях - авторматически (как здесь) драйверы не находятся
+
         try {
             con = DriverManager.getConnection(URL, USER, PASSWORD);
+//            con = DriverManager.getConnection(
+//                    Util.get(URL_KEY),
+//                    Util.get(USER_KEY),
+//                    Util.get(PASSWORD_KEY)
+//            );
             System.out.println(con.isClosed());
         } catch (SQLException e) {  // database access error || method is called on a closed connection
             System.out.println("Error: Unable to Connect to Database.");
